d0319ca9632a3ec07ecf120b01001d8cde50f311
Manisha3112 <greenyforever31@gmail.com>
2021-08-04 10:30:25 +0530

    First commit

--- app1.js
+++ app1.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const crudProduct = require('./app/routes/crudProduct')
-
 const dotenv = require('dotenv').config();
 const app = express();
-
 const port =3000
 app.use('/',crudProduct)
 app.listen(port, () => console.log(`listening on port ${port}`));
