2ac774e055cacb8f6cdd1969c02b1638efa58a62
Manisha3112 <greenyforever31@gmail.com>
2021-08-04 10:22:16 +0530

    First commit

--- app/utils/database.js
+++ app/utils/database.js
@@ -1,8 +1,7 @@
 const {MongoClient} = require('mongodb');
-
 async function connectDB() {
     try{
-        const uri = "mongodb+srv://root:root@cluster0.5r096.mongodb.net/shop?retryWrites=true&w=majority"
+        const mongoUrl = "mongodb+srv://root:root@cluster0.5r096.mongodb.net/shop?retryWrites=true&w=majority"
         return await MongoClient.connect();
     } catch (err) {
         console.log(err);
